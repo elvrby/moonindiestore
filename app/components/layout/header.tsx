df6e8468cb0e16c955860c256459535b2265738b
Alfarabx <alfarabi1074@gmail.com>
2025-04-16 14:01:16 +0700

    V 2.2

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Link from "next/link";
-
+import MobileComponent from '../addons/mobileheader';
 
 const HeaderComponent: React.FC = () => {
   const [navOpen, setNavOpen] = useState(false);
@@ -31,7 +31,7 @@ const HeaderComponent: React.FC = () => {
 
   return (
     <header className="w-full navbar h-14 flex items-center justify-center z-50">
-      <div className={`fixed top-0 w-full flex p-5 lg:flex h-16 items-center justify-between lg:pl-40 lg:pr-40 z-20 transition-colors duration-300 ${isScrolled ? "navbar bg-zinc-900 shadow-md" : "bg-transparent"}`}>
+      <div className={`md:fixed hidden top-0 w-full p-5 lg:flex h-16 items-center justify-between lg:pl-40 lg:pr-40 z-20 transition-colors duration-300 ${isScrolled ? "navbar bg-zinc-900 shadow-md" : "bg-transparent"}`}>
         {/* Logo di kiri */}
         <div className="flex-shrink-0">
           <Link href="/"> 
@@ -48,63 +48,15 @@ const HeaderComponent: React.FC = () => {
           <svg height={"30px"} widths={"20px"} fill="white" id="Layer_1" version="1.1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M344.5,298c15-23.6,23.8-51.6,23.8-81.7c0-84.1-68.1-152.3-152.1-152.3C132.1,64,64,132.2,64,216.3  c0,84.1,68.1,152.3,152.1,152.3c30.5,0,58.9-9,82.7-24.4l6.9-4.8L414.3,448l33.7-34.3L339.5,305.1L344.5,298z M301.4,131.2  c22.7,22.7,35.2,52.9,35.2,85c0,32.1-12.5,62.3-35.2,85c-22.7,22.7-52.9,35.2-85,35.2c-32.1,0-62.3-12.5-85-35.2  c-22.7-22.7-35.2-52.9-35.2-85c0-32.1,12.5-62.3,35.2-85c22.7-22.7,52.9-35.2,85-35.2C248.5,96,278.7,108.5,301.4,131.2z"/></svg>
         </nav>
 
-        {/* Tombol hamburger untuk tampilan mobile */}
-        <div className="md:hidden">
-          <button onClick={handleToggle} className="focus:outline-none">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              {navOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              )}
-            </svg>
-          </button>
-        </div>
+        
+
       </div>
 
-      {/* Menu navigasi untuk tampilan mobile */}
-      {navOpen && (
-        <nav className="fixed md:hidden w-full">
-          <ul className="flex flex-col items-center space-y-4 pb-4">
-            <li>
-              <Link href="/" onClick={() => setNavOpen(false)}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/products" onClick={() => setNavOpen(false)}>
-                Products
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact" onClick={() => setNavOpen(false)}>
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link href="/login" onClick={() => setNavOpen(false)}>
-                Login
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      )}
+      <div>
+          <MobileComponent></MobileComponent>
+        </div>
+
+      
     </header>
   );
 };
